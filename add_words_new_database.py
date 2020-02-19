63269742d0fe670616f9a0df705bacc18b10d73e
fmaj7-dev <eturegano@gmail.com>
2020-02-19 22:29:48 +0100

    updated to B1

--- add_words_new_database.py
+++ add_words_new_database.py
@@ -8,8 +8,9 @@ import codecs
 infile = "words.txt"
 
 # first free id in vocabulary database
-vocabulary_offset = 810
+vocabulary_offset = 1075
 user_id = 1
+category_id = 1
 
 with codecs.open(infile, encoding='utf-8') as inf:
     for line in inf:
@@ -17,9 +18,9 @@ with codecs.open(infile, encoding='utf-8') as inf:
         french_replaced = v[0].replace('\'', '\\\'')
         spanish_replaced = v[1].replace('\n', '') 
 
-        print("INSERT INTO `vocabulary` VALUES ("+str(vocabulary_offset)+", '"+french_replaced+"', '"+spanish_replaced+"', 2, 1, 2);")
+        print("INSERT INTO `vocabulary` VALUES ("+str(vocabulary_offset)+", '"+french_replaced+"', '"+spanish_replaced+"', 2, 1, 3);")
         print("INSERT INTO `guess` (`user_id`, `vocabulary_id`, `a2b_ok`,`a2b_ko`,`b2a_ok`,`b2a_ko`) VALUES ("+str(user_id)+", "+str(vocabulary_offset)+", 0, 0, 0, 0);")
-        print("INSERT INTO `vocabulary_category` (`vocabulary_id`, `category_id`) VALUES ("+str(vocabulary_offset)+", 2);")
+        print("INSERT INTO `vocabulary_category` (`vocabulary_id`, `category_id`) VALUES ("+str(vocabulary_offset)+", "+str(category_id)+");")
 
         vocabulary_offset += 1
 
